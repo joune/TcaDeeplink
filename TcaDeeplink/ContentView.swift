2d2baa3ad9b240bfa5a63ccc1f3c9caee5e9130e
Arjun Panday <arjun@tape.audio>
2024-08-06 10:43:43 +0200

    Initial Commit

---
+++ TcaDeeplink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TcaDeeplink
+//
+//  Created by Arjun Panday on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TcaDeeplink/TcaDeeplinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcaDeeplinkApp.swift
+//  TcaDeeplink
+//
+//  Created by Arjun Panday on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TcaDeeplinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
